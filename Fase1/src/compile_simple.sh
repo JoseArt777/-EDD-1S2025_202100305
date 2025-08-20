48c82f681bac986ecdf3d0a57db19f25f4426333
JoseArt777 <al40031897@gmail.com>
2025-08-20 17:31:29 -0600

    [ADD]

--- Fase1/src/compile_simple.sh
+++ Fase1/src/compile_simple.sh
@@ -20,9 +20,18 @@ fpc -Mobjfpc -Fu. units/FileManager.pas
 fpc -Mobjfpc -Fu. units/ReportGenerator.pas
 
 echo "Compilando interfaces..."
+fpc -Mobjfpc -Fu. ui/UIBase.pas
 fpc -Mobjfpc -Fu. ui/LoginWindow.pas
 fpc -Mobjfpc -Fu. ui/RootWindow.pas
 fpc -Mobjfpc -Fu. ui/UserWindow.pas
+fpc -Mobjfpc -Fu. ui/InboxWindow.pas
+fpc -Mobjfpc -Fu. ui/ComposeWindow.pas
+fpc -Mobjfpc -Fu. ui/ContactWindow.pas
+fpc -Mobjfpc -Fu. ui/TrashWindow.pas
+fpc -Mobjfpc -Fu. ui/ScheduleWindow.pas
+fpc -Mobjfpc -Fu. ui/ScheduledWindow.pas
+fpc -Mobjfpc -Fu. ui/ProfileWindow.pas
+fpc -Mobjfpc -Fu. ui/ReportsWindow.pas
 
 echo "Compilando programa principal..."
 fpc -Mobjfpc -Fu. -o../bin/eddmail EDDMail.pas
